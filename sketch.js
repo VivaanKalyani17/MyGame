2b48d771c67d9e6308374f74740ea81ac2998988
VivaanKalyani17 <vivaankalyani@gmail.com>
2021-04-24 10:51:52 +0530

    hello

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+var gameState ="PLAY"
 function preload(){
 
 shark=loadAnimation("Shark1.gif","Shark2.gif","Shark3.gif","Shark4.gif")
@@ -7,6 +8,7 @@ bg=loadAnimation("Back1.gif","Back2.gif","Back3.gif","Back4.gif")
 jellyfish=loadAnimation("Jellyfish1.gif","Jellyfish2.gif","Jellyfish3.gif","Jellyfish4.gif","Jellyfish5.gif")
 rock=loadImage("Rock.png")
 seaUrchin=loadImage("Jellyfish.png")
+gameOver=loadImage("GameOver.jpg")
 }
 function setup(){
 createCanvas(1000,600)
@@ -17,11 +19,16 @@ createCanvas(1000,600)
   myFish=createSprite(200,500)
   myFish.scale=0.5
   myFish.addAnimation("Fish",fish)
-
+sharkGroup=new Group()
+jellyfishGroup=new Group()
+over=createSprite(500,300)
+over.addImage("N",gameOver)
+over.visible=false
  
   
 
 }function draw(){
+  if (gameState=="PLAY"){
 if(myBg.x<450){
 myBg.x=myBg.width/2
 
@@ -38,7 +45,25 @@ if(keyDown("down")&&myFish.y<1000){
     if(keyDown("left")&&myFish.x>0){
       myFish.x=myFish.x-2
       }
-spawnJellyfish()
+      if(keyDown("S")){
+        spawnShield()
+      }
+      if(myFish.isTouching(sharkGroup)||myFish.isTouching(jellyfishGroup)){
+        gameState="END"
+
+      }
+      
+      spawnShark()
+spawnJellyfish()}
+else {
+  myFish.visible=false
+        jellyfishGroup.destroyEach()
+        sharkGroup.destroyEach()
+       
+        myBg.velocityX=0
+        over.visible=true
+        over.scale=1.5
+}
   drawSprites()
 
 }function spawnJellyfish(){
@@ -48,6 +73,26 @@ if(frameCount%150===0){
 jelly.addAnimation("jelly",jellyfish)
 jelly.scale=0.3
 jelly.lifetime=1000
+jellyfishGroup.add(jelly)
 
 }
+}
+// //function spawnShield(){
+//   shields=createSprite(myFish.x,myFish.y,10,10)
+//   shields.addAnimation("shields",shield)
+//   shield.scale=0.5
+//   shield.lifetime=10
+// }
+function spawnShark(){
+  
+  rand = Math.round(random(300,400))
+  if(frameCount%rand==0){
+
+myShark=createSprite(random(0,1000),random(0,600))
+myShark.addAnimation("Sharks",shark)
+myShark.velocityX=Math.round(random(0,6))
+myShark.velocityY=Math.round(random(0,6))
+myShark.lifetime=600
+sharkGroup.add(myShark)
+  }
 }
