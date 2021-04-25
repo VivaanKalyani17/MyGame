e1824eb804714645617a88f638e5f7284a0079fb
VivaanKalyani17 <vivaankalyani@gmail.com>
2021-04-25 10:58:14 +0530

    hhh

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+var score = 0
+var life = 0
 var gameState ="PLAY"
 function preload(){
 
@@ -9,6 +11,7 @@ jellyfish=loadAnimation("Jellyfish1.gif","Jellyfish2.gif","Jellyfish3.gif","Jell
 rock=loadImage("Rock.png")
 seaUrchin=loadImage("Jellyfish.png")
 gameOver=loadImage("GameOver.jpg")
+food = loadAnimation("Food 1.gif","Food 2.gif","Food 3.gif","Food 4.gif","Food 5.gif")
 }
 function setup(){
 createCanvas(1000,600)
@@ -24,6 +27,7 @@ jellyfishGroup=new Group()
 over=createSprite(500,300)
 over.addImage("N",gameOver)
 over.visible=false
+FishFood = new Group()
  
   
 
@@ -52,19 +56,40 @@ if(keyDown("down")&&myFish.y<1000){
         gameState="END"
 
       }
-      
+      if(frameCount%500===0){
+        jellyfishGroup.setVelocityXEach(-10)
+        console.log(jellyfishGroup)
+        for(var i=0;i<jellyfishGroup.length;i++){
+          jellyfishGroup[i].velocity.x-=2
+        }
+      }
+      spawnFood()
+      if(myFish.isTouching(FishFood)){
+        score=score+1
+        FishFood.destroyEach()
+      }
+      life=life+Math.round(frameCount/60)
+      fill("black")
+
       spawnShark()
 spawnJellyfish()}
 else {
   myFish.visible=false
         jellyfishGroup.destroyEach()
         sharkGroup.destroyEach()
+        FishFood.destroyEach()
+        fill("white")
+      
        
         myBg.velocityX=0
         over.visible=true
         over.scale=1.5
 }
   drawSprites()
+  textSize(25)
+  
+text("Score:"+score,800,50)
+text("Lifetime:"+life,800,100)
 
 }function spawnJellyfish(){
 if(frameCount%150===0){
@@ -74,6 +99,8 @@ jelly.addAnimation("jelly",jellyfish)
 jelly.scale=0.3
 jelly.lifetime=1000
 jellyfishGroup.add(jelly)
+
+jelly.setCollider("rectangle",0,0,20,20)
 
 }
 }
@@ -94,5 +121,22 @@ myShark.velocityX=Math.round(random(0,6))
 myShark.velocityY=Math.round(random(0,6))
 myShark.lifetime=600
 sharkGroup.add(myShark)
+myShark.setCollider("rectangle",0,0,50,50)
+  }
+}
+function spawnFood(){
+  
+  
+  if(frameCount%150==0){
+
+myFood=createSprite(random(0,1000),random(0,600))
+myFood.addAnimation("food",food)
+
+myFood.lifetime=250
+FishFood.add(myFood)
+
+
+myFood.setCollider("rectangle",0,0,20,20)
+
   }
 }
